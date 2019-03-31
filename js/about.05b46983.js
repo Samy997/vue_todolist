350e0d43f16a6ae5990b381295e237e28a79fc93
samy997 <zamy.11.4.1997@gmail.com>
2019-03-31 12:46:14 +0200

    Updates

---
+++ js/about.05b46983.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,s){"use strict";s.r(e);var n=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},u=[function(){var t=this,e=t.$createElement,s=t._self._c||e;return s("div",{staticClass:"about"},[s("h1",[t._v("About")]),s("p",[t._v("This Is The TodoList App v1.0.0. It Is Part Of The Vue Crash Course")])])}],a=s("2877"),r={},o=Object(a["a"])(r,n,u,!1,null,null,null);e["default"]=o.exports}}]);
+//# sourceMappingURL=about.05b46983.js.map
